0373c6c49e35696b0beba6cac82cf6c04de70e52
Michael Suarez <ozmasv@hotmail.com>
2022-08-18 22:51:02 -0500

    Oz

---
+++ src/main/java/gov/mintic/COVENANT/TrabajoEmpresa/TrabajoEmpresaApplication.java
@@ -0,0 +1,13 @@
+package gov.mintic.COVENANT.TrabajoEmpresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoEmpresaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/mintic/COVENANT/TrabajoEmpresa/TrabajoEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.mintic.COVENANT.TrabajoEmpresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
