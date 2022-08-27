fb30b1c55a16d46d402d37aae968781e628f04af
JARiverSua <jars167@hotmail.com>
2022-08-26 23:39:14 -0500

    clases

---
+++ src/main/java/gov/mintic/COVENANT/TrabajoEmpresa/Entity/Enterprise.java
@@ -0,0 +1,83 @@
+package gov.mintic.COVENANT.TrabajoEmpresa.Entity;
+
+import java.util.Arrays;
+
+public class Enterprise {
+
+    private long idEmpresa;
+    private String nombres;
+    private String documento;
+    private String numeroTelefono;
+    private String direccion;
+    private Employee empleado[];
+    private Transactions tansacciones[];
+
+    public long getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(long idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Employee[] getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Employee[] empleado) {
+        this.empleado = empleado;
+    }
+
+    public Transactions[] getTansacciones() {
+        return tansacciones;
+    }
+
+    public void setTansacciones(Transactions[] tansacciones) {
+        this.tansacciones = tansacciones;
+    }
+
+    @Override
+    public String toString() {
+        return "Enterprise{" +
+                "idEmpresa=" + idEmpresa +
+                ", nombres='" + nombres + '\'' +
+                ", documento='" + documento + '\'' +
+                ", numeroTelefono='" + numeroTelefono + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", empleado=" + Arrays.toString(empleado) +
+                ", tansacciones=" + Arrays.toString(tansacciones) +
+                '}';
+    }
+}
